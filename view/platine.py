4b9d09ce206b9002cb3948ef2936e66521a96929
Nicolas Guy <2439653@cmontmorency.qc.ca>
2025-04-23 07:08:25 -0400

    [Fonctionnalite]: <ajout des boutons sur la platine et test pour les boutons>

--- view/platine.py
+++ view/platine.py
@@ -1,1 +1,14 @@
 from gpiozero import Button
+from time import sleep
+
+btn_vert = Button(17)
+btn_rouge = Button(27)
+
+while True:
+    if btn_vert.is_pressed:
+        print("vert")   
+        sleep(0.5)
+
+    if btn_rouge.is_pressed:
+        print("rouge")    
+        sleep(0.5)
