57e3782b4360de3187c6439057c9ffc18f6d8b37
Nicolas Guy <2439653@cmontmorency.qc.ca>
2025-04-23 08:50:37 -0400

    [Fonctionnalite]: <ajout  du capteurde distance et code dans la console>

--- view/platine.py
+++ view/platine.py
@@ -1,8 +1,12 @@
-from gpiozero import Button
+from gpiozero import Button, DistanceSensor
 from time import sleep
 
-btn_vert = Button(17)
+btn_vert = Button(5)
 btn_rouge = Button(27)
+
+capteur = DistanceSensor(echo =12,
+                         trigger = 17,
+                         max_distance = 3)
 
 while True:
     if btn_vert.is_pressed:
@@ -12,3 +16,7 @@ while True:
     if btn_rouge.is_pressed:
         print("rouge")    
         sleep(0.5)
+
+    cm = capteur.distance * 100
+    print("Distance: " + str(cm) + " cm")
+    sleep(1)
